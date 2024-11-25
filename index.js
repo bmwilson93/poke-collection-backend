f8c8dd7e0bae23e5452050c99a402a98d5e9d541
Brian Wilson <bmwilson93@gmail.com>
2024-11-25 03:00:25 -0700

    added import and router for the session router

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
+// Router Imports
+const sessionRouter = require('./routes/session.js');
+
 const PORT = process.env.PORT || 4001;
 
-app.get('/', (req, res) => {
-  res.send("hi");
-})
+// Routers
+app.use(sessionRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
