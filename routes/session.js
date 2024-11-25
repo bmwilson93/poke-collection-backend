880212a0281c3063d1531f1d2f98581319b970c9
Brian Wilson <bmwilson93@gmail.com>
2024-11-25 03:02:07 -0700

    created and added connection and session configuration to session.js

---
+++ routes/session.js
@@ -0,0 +1,24 @@
+const express = require('express');
+      router = express.Router();
+require('dotenv').config();
+
+// Sessions
+const session = require('express-session');
+const db = require('../database/db.js');
+const pgSession = require('connect-pg-simple')(session);
+
+router.use(session({
+  store: new pgSession({
+    pool: db,
+    tableName: 'session'
+  }),
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    sameSite: "none",
+    secure: true
+  },
+  resave: false,
+  saveUninitialized: false
+}));
+
+module.exports = router;
