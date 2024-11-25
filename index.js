adc73b30cd7699f6ec6c338f74b5fb57500af2c4
Brian Wilson <bmwilson93@gmail.com>
2024-11-25 02:18:27 -0700

    added dotenv config

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 
-const PORT = 4001;
+const PORT = process.env.PORT || 4001;
 
 app.get('/', (req, res) => {
   res.send("hi");
