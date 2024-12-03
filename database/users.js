1283c164eed281558dea5ebf2b0e5265bb357395
Brian Wilson <bmwilson93@gmail.com>
2024-12-03 05:01:46 -0700

    adding db queries to insert user and collection for new user, returns the new user object

--- database/users.js
+++ database/users.js
@@ -13,6 +13,20 @@ const findUser = async (email) => {
 
 const addUser = async (user) => {
   try {
+    // query to add a row to users, and a row to collections
+    const newUser = await db.query(
+      'INSERT INTO users (email, username, password) VALUES ($1, $2, $3) RETURNING id, email, username, password;',
+      [user.email, user.username, user.password]
+    );
+    newUser = newUser.rows[0];
+
+    const newCollection = await db.query(
+      'INSERT INTO collections (collection, user_id) VALUES ($1, $2) RETURNING collection;',
+      ['{"sets":[]}', newUser.id]
+    );
+    newUser.collection = newCollection.rows[0].collection;
+
+    return newUser;
     
   } catch (error) {
     console.log(error);
