76fe89c2f511644b9e66026f97896dbb26f82d4b
Brian Wilson <bmwilson93@gmail.com>
2024-12-03 05:20:54 -0700

    imported and use passport router, added use of express.json to parse json body

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ require('dotenv').config();
 
 // Router Imports
 const sessionRouter = require('./routes/session.js');
+const passportRouter = require('./routes/passport.js');
 
 const PORT = process.env.PORT || 4001;
 
+app.use(express.json());
+
 // Routers
 app.use(sessionRouter);
+app.use(passportRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
