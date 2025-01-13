5706dec6a383e0c884e344f418775143953000c8
Brian Wilson <bmwilson93@gmail.com>
2025-01-13 06:11:38 -0700

    updated to check for is_production env variable, to prevent having to keep changing pool settings between local dev and delpoyment

--- database/db.js
+++ database/db.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  // host: process.env.DB_HOST,
-  // ssl: true, // comment when using localhost
-  // port: process.env.DB_PORT,
-  // user: process.env.DB_USER,
-  // database: process.env.DB_DATABASE,
-  // password: process.env.DB_PASSWORD
-  connectionString: process.env.DB_CONNECTION_STRING
-});
+const pool = process.env.IS_PRODUCTION === 'true'
+  ? new Pool({connectionString: process.env.DB_CONNECTION_STRING}) 
+  : new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+  })
 
 pool.connect();
 
