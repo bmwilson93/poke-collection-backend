d2e3eeae46c7fa2d6bed7262b37d2a8d2d228522
Brian Wilson <bmwilson93@gmail.com>
2025-01-21 02:28:55 -0700

    Changed to call session with different settings depending on production

--- routes/session.js
+++ routes/session.js
@@ -7,7 +7,9 @@ const session = require('express-session');
 const db = require('../database/db.js');
 const pgSession = require('connect-pg-simple')(session);
 
-router.use(session({
+router.use(session(
+  process.env.IS_PRODUCTION == 'true' 
+  ? {
   store: new pgSession({
     pool: db,
     tableName: 'session'
@@ -22,6 +24,23 @@ router.use(session({
   },
   resave: false,
   saveUninitialized: false,
-}));
+}
+: { // Not production, using localhost
+  store: new pgSession({
+    pool: db,
+    tableName: 'session'
+  }),
+  secret: process.env.SESSION_SECRET,
+  proxy: true,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 90,
+    sameSite: "lax",
+    secure: false,
+    httpOnly: false,
+  },
+  resave: false,
+  saveUninitialized: false,
+}
+));
 
 module.exports = router;
