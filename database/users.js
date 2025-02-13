843f2b926a7964e3464f8699cb0b338c533081d6
Brian Wilson <bmwilson93@gmail.com>
2025-02-13 05:21:09 -0700

    Updated to fix some issues with user.id missing from object. Now uses email to select the correct user record instead

--- database/users.js
+++ database/users.js
@@ -39,23 +39,26 @@ const updateUserEmail = async (user, newEmail) => {
 try {
   // Update the user email
   let result = await db.query(
-    'UPDATE users SET email = $1 WHERE id = $2;',
-    [newEmail, user.id]
+    'UPDATE users SET email = $1 WHERE email = $2;',
+    [newEmail, user.email]
   );
-  if (!result) return null;
+  if (result.rowCount) {
+    // Get the updated user and the collection to return
+    let updatedUser = await db.query(
+      'SELECT email, username, collection FROM users JOIN collections ON users.id = collections.user_id WHERE users.email=$1;',
+      [newEmail]
+    );
+    updatedUser = updatedUser.rows[0];
+    return updatedUser;
+  } else {
+    return null;
+  }
 
-  // Get the updated user and the collection to return
-  let updatedUser = await db.query(
-    'SELECT email, username, collection FROM users JOIN collections ON users.id = collections.user_id WHERE users.id=$1;',
-    [user.id]
-  );
-  updatedUser = updatedUser.rows[0];
-  return updatedUser;
-  
+
 } catch (error) {
   console.log(error);
   return null;
 }
 }
 
-module.exports = { findUser, addUser }
+module.exports = { findUser, addUser, updateUserEmail }
