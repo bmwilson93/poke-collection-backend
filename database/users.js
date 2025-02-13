08a2a4357abd0307d0e8b56ff0c4ab2c13364524
Brian Wilson <bmwilson93@gmail.com>
2025-02-13 03:37:48 -0700

    Completed the updateUserEmail function. Runs an update query, confirms update, then gets the updated user and collection, and joins before returning user object

--- database/users.js
+++ database/users.js
@@ -36,7 +36,26 @@ const addUser = async (user) => {
 }
 
 const updateUserEmail = async (user, newEmail) => {
+try {
+  // Update the user email
+  let result = await db.query(
+    'UPDATE users SET email = $1 WHERE id = $2;',
+    [newEmail, user.id]
+  );
+  if (!result) return null;
 
+  // Get the updated user and the collection to return
+  let updatedUser = await db.query(
+    'SELECT email, username, collection FROM users JOIN collections ON users.id = collections.user_id WHERE users.id=$1;',
+    [user.id]
+  );
+  updatedUser = updatedUser.rows[0];
+  return updatedUser;
+  
+} catch (error) {
+  console.log(error);
+  return null;
+}
 }
 
 module.exports = { findUser, addUser }
